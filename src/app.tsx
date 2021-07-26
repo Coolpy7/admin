569bc7f580810f8f1f1e3a7c1d79c0d2946bd952
黎东海 <jacle169@21cn.com>
2021-07-26 13:28:50 +0800

    刷新路由修正

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,11 @@
-import type { Settings as LayoutSettings } from '@ant-design/pro-layout';
-import { PageLoading } from '@ant-design/pro-layout';
-import { notification } from 'antd';
-import type { RequestConfig, RunTimeLayoutConfig } from 'umi';
-import { history } from 'umi';
+import type {Settings as LayoutSettings} from '@ant-design/pro-layout';
+import {PageLoading} from '@ant-design/pro-layout';
+import {notification} from 'antd';
+import type {RequestConfig, RunTimeLayoutConfig} from 'umi';
+import {history} from 'umi';
 import RightContent from '@/components/RightContent';
 import Footer from '@/components/Footer';
-import { currentUser as queryCurrentUser } from './services/ant-design-pro/api';
+import {currentUser as queryCurrentUser} from './services/ant-design-pro/api';
 import defaultSettings from '@ant-design/pro-layout/lib/defaultSettings';
 
 const isDev = process.env.NODE_ENV === 'development';
@@ -13,7 +13,7 @@ const loginPath = '/user/login';
 
 /** 获取用户信息比较慢的时候会展示一个 loading */
 export const initialStateConfig = {
-  loading: <PageLoading />,
+  loading: <PageLoading/>,
 };
 
 /**
@@ -35,7 +35,7 @@ export async function getInitialState(): Promise<{
     return undefined;
   };
   let ip = localStorage.getItem('ip');
-  if(ip == undefined || ip === "") {
+  if (ip == undefined || ip === "") {
     return {
       fetchUserInfo,
       settings: defaultSettings,
@@ -58,40 +58,40 @@ export async function getInitialState(): Promise<{
 
 /**
  * 异常处理程序
-    200: '服务器成功返回请求的数据。',
-    201: '新建或修改数据成功。',
-    202: '一个请求已经进入后台排队（异步任务）。',
-    204: '删除数据成功。',
-    400: '发出的请求有错误，服务器没有进行新建或修改数据的操作。',
-    401: '用户没有权限（令牌、用户名、密码错误）。',
-    403: '用户得到授权，但是访问是被禁止的。',
-    404: '发出的请求针对的是不存在的记录，服务器没有进行操作。',
-    405: '请求方法不被允许。',
-    406: '请求的格式不可得。',
-    410: '请求的资源被永久删除，且不会再得到的。',
-    422: '当创建一个对象时，发生一个验证错误。',
-    500: '服务器发生错误，请检查服务器。',
-    502: '网关错误。',
-    503: '服务不可用，服务器暂时过载或维护。',
-    504: '网关超时。',
+ 200: '服务器成功返回请求的数据。',
+ 201: '新建或修改数据成功。',
+ 202: '一个请求已经进入后台排队（异步任务）。',
+ 204: '删除数据成功。',
+ 400: '发出的请求有错误，服务器没有进行新建或修改数据的操作。',
+ 401: '用户没有权限（令牌、用户名、密码错误）。',
+ 403: '用户得到授权，但是访问是被禁止的。',
+ 404: '发出的请求针对的是不存在的记录，服务器没有进行操作。',
+ 405: '请求方法不被允许。',
+ 406: '请求的格式不可得。',
+ 410: '请求的资源被永久删除，且不会再得到的。',
+ 422: '当创建一个对象时，发生一个验证错误。',
+ 500: '服务器发生错误，请检查服务器。',
+ 502: '网关错误。',
+ 503: '服务不可用，服务器暂时过载或维护。',
+ 504: '网关超时。',
  //-----English
-    200: The server successfully returned the requested data. ',
-    201: New or modified data is successful. ',
-    202: A request has entered the background queue (asynchronous task). ',
-    204: Data deleted successfully. ',
-    400: 'There was an error in the request sent, and the server did not create or modify data. ',
-    401: The user does not have permission (token, username, password error). ',
-    403: The user is authorized, but access is forbidden. ',
-    404: The request sent was for a record that did not exist. ',
-    405: The request method is not allowed. ',
-    406: The requested format is not available. ',
-    410':
-        'The requested resource is permanently deleted and will no longer be available. ',
-    422: When creating an object, a validation error occurred. ',
-    500: An error occurred on the server, please check the server. ',
-    502: Gateway error. ',
-    503: The service is unavailable. ',
-    504: The gateway timed out. ',
+ 200: The server successfully returned the requested data. ',
+ 201: New or modified data is successful. ',
+ 202: A request has entered the background queue (asynchronous task). ',
+ 204: Data deleted successfully. ',
+ 400: 'There was an error in the request sent, and the server did not create or modify data. ',
+ 401: The user does not have permission (token, username, password error). ',
+ 403: The user is authorized, but access is forbidden. ',
+ 404: The request sent was for a record that did not exist. ',
+ 405: The request method is not allowed. ',
+ 406: The requested format is not available. ',
+ 410':
+ 'The requested resource is permanently deleted and will no longer be available. ',
+ 422: When creating an object, a validation error occurred. ',
+ 500: An error occurred on the server, please check the server. ',
+ 502: Gateway error. ',
+ 503: The service is unavailable. ',
+ 504: The gateway timed out. ',
  * @see https://beta-pro.ant.design/docs/request-cn
  */
 
@@ -115,11 +115,16 @@ export async function getInitialState(): Promise<{
 let url = 'http://192.168.101.180:8081';
 if (localStorage.getItem('ip')) {
   url = ' http://' + localStorage.getItem('ip');
+} else {
+  const Url = window.location.href;
+  let splitUrl = Url.split('://')[1];
+  url = splitUrl.split('/')[0];
+  localStorage.setItem('ip', url);
 }
 export const request: RequestConfig = {
   timeout: 2000,
   errorHandler: (error: any) => {
-    const { response } = error;
+    const {response} = error;
     if (!response) {
       notification.error({
         description: '您的网络发生异常，无法连接服务器',
@@ -138,17 +143,17 @@ export const request: RequestConfig = {
 };
 
 // ProLayout 支持的api https://procomponents.ant.design/components/layout
-export const layout: RunTimeLayoutConfig = ({ initialState }) => {
+export const layout: RunTimeLayoutConfig = ({initialState}) => {
   return {
     title: 'Coolpy7 Admin',
-    rightContentRender: () => <RightContent />,
+    rightContentRender: () => <RightContent/>,
     disableContentMargin: false,
     // waterMarkProps: {
     //  content: initialState?.currentUser?.data.name,
     //  },
-    footerRender: () => <Footer />,
+    footerRender: () => <Footer/>,
     onPageChange: () => {
-      const { location } = history;
+      const {location} = history;
       // 如果没有登录，重定向到 login
       if (!initialState?.currentUser && location.pathname !== loginPath) {
         history.push(loginPath);
@@ -156,17 +161,17 @@ export const layout: RunTimeLayoutConfig = ({ initialState }) => {
     },
     links: isDev
       ? [
-          /*
-          <Link to="/umi/plugin/openapi" target="_blank">
-            <LinkOutlined />
-            <span>openAPI 文档</span>
-          </Link>,
-          <Link to="/~docs">
-            <BookOutlined />
-            <span>业务组件文档</span>
-          </Link>,
-          */
-        ]
+        /*
+        <Link to="/umi/plugin/openapi" target="_blank">
+          <LinkOutlined />
+          <span>openAPI 文档</span>
+        </Link>,
+        <Link to="/~docs">
+          <BookOutlined />
+          <span>业务组件文档</span>
+        </Link>,
+        */
+      ]
       : [],
     menuHeaderRender: undefined,
     // 自定义 403 页面
